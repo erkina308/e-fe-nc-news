d001262d7afa22f83f2e7ccb559063adc0412ae7
erkina308 <erkinayna@hotmail.co.uk>
2024-02-06 09:56:15 +0000

    this commit contains the article by id section and comments resolved except the one about the users

--- src/cards/ArticleCard.jsx
+++ src/cards/ArticleCard.jsx
@@ -1,13 +1,24 @@
 import Button from "../styleComponents/Button";
+import StyledLink from "../styleComponents/Link";
 
 function ArticleCard({ children }) {
-  const { article_img_url, author, body, created_at, title, votes, topic } =
-    children;
+  const {
+    article_img_url,
+    author,
+    body,
+    created_at,
+    title,
+    votes,
+    topic,
+    article_id,
+  } = children;
   return (
     <section>
       <div>
         <h2>{author}</h2>
-        <img src={article_img_url} />
+        <StyledLink to={`/api/articles/${article_id}`}>
+          <img src={article_img_url} />
+        </StyledLink>
         <p>{title}</p>
         <p>{body}</p>
         <p>{created_at}</p>
